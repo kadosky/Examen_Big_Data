63ff33b476455f235d97bf6645e44abafd80439c
Kadosky <diop4060@gmail.com>
2021-05-20 12:29:50 +0000

    maj 2

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,3 +1,5 @@
+import scala.collection.breakOut
+import scala.collection.immutable.ListMap
 object ExamenBigData {
 
   def ModifList(liste:List[String]): List[String] =  {
@@ -5,7 +7,33 @@ object ExamenBigData {
     return listeFinal
   }
 
-  def main(args: Array[String]): Unit = {
-
+  def avoirdomaine(lien:String) : String ={ // elle permet d'avoir les domaines des liens
+    return lien.replace("https://www.","").split("/")(0)
   }
+
+  def liste_double(): Unit ={
+    val Maliste = List(("ecommercemag.fr"," "),
+      ("https://www.journalducm.com/contact/","Payant"),
+      ("https://www.zatsaz.com/"," "),
+      ("https://www.lerevenu.com/"," "),
+      ("https://www.cadre-dirigfdeant-magazine.com/","Payant"),
+      ("https://www.silicon.fr/services/contact#annoner","Payant"),
+      ("https://www.channelbiz.fr/nous-contacter/"," "),
+      ("https://www.itespresso.fr/"," "),
+      ("https://www.industrie-mag.com/article4.html","invite"),
+      ("https://www.jesuisundev.com/article-invite/","invite"),
+      ("https://www.numerama.com/"," ")
+    )
+    val finalListe =  (ListMap(Maliste:_*).keys.toList.map(l => avoirdomaine(l) ) zip ListMap(Maliste:_*).values).toList // funsionner les 2 dans une liste de tuple
+
+    println(finalListe)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val listetest = List("julien","Paul", "jean", "rac", "trec", "joel", "ed","chris", "maurice")
+    println(ModifList(listetest))
+    liste_double()
+  }
+
+
 }
